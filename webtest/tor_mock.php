6928afe91102eca663992247d3459065fd87d849
Matteo Nastasi (mop) <nastasi@alternativeoutput.it>
2015-01-14 00:23:51 +0100

    updated async counter

--- webtest/tor_mock.php
+++ webtest/tor_mock.php
@@ -6,6 +6,7 @@ $dest_port = $_POST['DestinationPort'];
 
 printf("QUERY_IP: [%s]\n", $query_ip);
 
+// if ($query_ip == "192.168.122.1") {
 if ($query_ip == "178.162.193.213") {
     // <td class="TRC"><br><br><b><font color="#00dd00">-The IP Address you entered matches one or more active Tor servers-</font><br><br>Server name: <a class="plain" href="http://torstatus.blutmagie.de/router_detail.php?FP=89e3170b4e2fc9a430fb97536769fc0abf6c4db3">hviv103</a><br><br></b></td>
 
